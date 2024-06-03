104b84457468c8c1ebe98ed18be4cedb30abda24
aced0002 <acedrust@gmail.com>
2024-06-03 03:37:29 +0100

    Aced 3.6.24
    I converted a boolean to a string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+             This Program
+             Converts
+             A Boolean
+             To
+             A
+             String
+             */
+            
+            bool Earth = false;
+
+            Console.WriteLine(Earth.ToString());
+        }
+    }
+}
